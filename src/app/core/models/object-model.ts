8f8086b2e467cf8715fdb524ae5e9ca67b3b32b9
Marko Stankovic <marefaca2002@gmail.com>
2024-06-11 22:25:15 +0200

    Complete filter functionality

--- src/app/core/models/object-model.ts
+++ src/app/core/models/object-model.ts
@@ -3,7 +3,10 @@ export class Product {
     name: string;
     category: string;
     brand: string;
-    price: any;
+    price: {
+        oldPrice: number | null;
+        activePrice: number;
+    };
     discount: number | null;
     description: string;
     sizes: string[];
@@ -11,10 +14,17 @@ export class Product {
     gender: string;
     inStock: boolean;
     images: string[];
-    reviews: any[];
+    reviews: Review[];
     averageRating: number;
 }
 export class NamedEntity{
     id: number;
     name: string;
+}
+export class Review{
+    id: number;
+    rating: number;
+    comment: string;
+    user: string;
+    avatar: string;
 }
--- src/app/pages/shop/components/shop/shop.component.ts
+++ src/app/pages/shop/components/shop/shop.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './shop.component.css'
 })
 export class ShopComponent implements OnInit{
-  protected products : Product[];
+  protected products : Product[] = [];
   protected originalProducts : Product[];
   protected colors : NamedEntity[];
   protected sizes : NamedEntity[];
@@ -24,14 +24,23 @@ export class ShopComponent implements OnInit{
     price: {
       min : 0,
       max : 1000
-    }
+    },
+    sortBy : ""
   };
-  protected selectedCategoryIndex : number = -1;
-  protected selectedBrandIndexes : number[] = [];
-  protected selectedSizeIndexes : number[] = [];
-  protected selectedColorIndexes : number[] = [];
-  protected selectedPriceIndex : number = 0;
-  private baseUrl = 'http://localhost:4200/assets/data';
+  protected selectedIndexes = {
+    colors : [],
+    sizes : [],
+    brands : [],
+    category : -1,
+    price : 0,
+
+  }
+  protected minProductIndex : number = 1;
+  protected maxProductIndex : number = 0;
+  protected productsLength : number = 0;
+  protected pages : number[] = [];
+  protected activePage : number = 1;
+  private baseUrl = 'http://localhost:4200/assets/data/';
 
  constructor(private productService : ProductsService,private http : HttpClient){
   
@@ -44,20 +53,26 @@ export class ShopComponent implements OnInit{
   loadProducts(){
     this.productService.getProducts().subscribe((data)=>{
       this.products = data;
+      this.maxProductIndex = this.products.slice((this.activePage-1)*8,this.activePage*8).length;
+      this.productsLength = this.products.length;
       this.originalProducts = data;
+      for(let i = 1; i <= Math.ceil(this.products.length/8); i++){
+        this.pages.push(i);
+      }
+      this.products = this.products.slice(0,8);
     });
   }
   loadFilters(){
-    this.http.get<NamedEntity[]>(`${this.baseUrl}/colors.json`).subscribe((data)=>{
+    this.http.get<NamedEntity[]>(`${this.baseUrl}colors.json`).subscribe((data)=>{
       this.colors = data;
     });
-    this.http.get<NamedEntity[]>(`${this.baseUrl}/sizes.json`).subscribe((data)=>{
+    this.http.get<NamedEntity[]>(`${this.baseUrl}sizes.json`).subscribe((data)=>{
       this.sizes = data;
     });
-    this.http.get<NamedEntity[]>(`${this.baseUrl}/brands.json`).subscribe((data)=>{
+    this.http.get<NamedEntity[]>(`${this.baseUrl}brands.json`).subscribe((data)=>{
       this.brands = data;
     });
-    this.http.get<NamedEntity[]>(`${this.baseUrl}/categories.json`).subscribe((data)=>{
+    this.http.get<NamedEntity[]>(`${this.baseUrl}categories.json`).subscribe((data)=>{
       this.categories = data;
     });
   }
@@ -67,10 +82,10 @@ export class ShopComponent implements OnInit{
   }
   filterByColor(color : string, index : number){
     if(this.filters.colors.includes(color)){ 
-      this.selectedColorIndexes = this.selectedColorIndexes.filter((item)=> item !== index);
+      this.selectedIndexes.colors = this.selectedIndexes.colors.filter((item)=> item !== index);
       this.filters.colors = this.filters.colors.filter((item)=> item !== color);
     }else{
-      this.selectedColorIndexes.push(index);
+      this.selectedIndexes.colors.push(index);
       this.filters.colors.push(color);
     }
     console.log(this.filters.colors);
@@ -79,42 +94,60 @@ export class ShopComponent implements OnInit{
   }
   filterBySize(size : string, index : number){
     if(this.filters.sizes.includes(size)){
-      this.selectedSizeIndexes = this.selectedSizeIndexes.filter((item)=> item !== index);
+      this.selectedIndexes.sizes = this.selectedIndexes.sizes.filter((item)=> item !== index);
       this.filters.sizes = this.filters.sizes.filter((item)=> item !== size);
     }else{
-      this.selectedSizeIndexes.push(index);
+      this.selectedIndexes.sizes.push(index);
       this.filters.sizes.push(size);
     }
     this.filterProducts();
   }
   filterByBrand(brand : string, index : number){
     if(this.filters.brands.includes(brand)){
-      this.selectedBrandIndexes = this.selectedBrandIndexes.filter((item)=> item !== index);
+      this.selectedIndexes.brands = this.selectedIndexes.brands.filter((item)=> item !== index);
       this.filters.brands = this.filters.brands.filter((item)=> item !== brand);
     }else{
-      this.selectedBrandIndexes.push(index);
+      this.selectedIndexes.brands.push(index);
       this.filters.brands.push(brand);
     }
     this.filterProducts();
   }
   filterByCategory(category : string, index : number){
-    if(this.selectedCategoryIndex === index){
-      this.selectedCategoryIndex = -1;
+    if(this.selectedIndexes.category === index){
+      this.selectedIndexes.category = -1;
       this.filters.category = "";
       this.filterProducts();
       return;
     }
-    this.selectedCategoryIndex = index;
+    this.selectedIndexes.category = index;
     this.filters.category = category;
     this.filterProducts();
   }
+  pagination(page : number){
+    this.activePage = page;
+    this.filterProducts(true);
+  }
   filterByPrice(index : number, min : number, max? : number){
-    this.selectedPriceIndex = index;
+    if(this.selectedIndexes.price === index){
+      this.selectedIndexes.price = 0;
+      this.filters.price.min = 0;
+      this.filters.price.max = 1000;
+      this.filterProducts();
+      return;
+    }
+    this.selectedIndexes.price = index;
     this.filters.price.min = min;
     this.filters.price.max = max || 1000;
     this.filterProducts();
   }
-  filterProducts(){
+  sortProducts(sortBy : string){
+    this.filters.sortBy = sortBy;
+    this.filterProducts();
+  }
+  filterProducts(paginationClicked? : boolean){
+    if(!paginationClicked){
+      this.activePage = 1;
+    }
     this.products = this.originalProducts.filter((product)=>{
       return product.name.toLowerCase().includes(this.filters.keyword.toLowerCase());
     });
@@ -155,6 +188,25 @@ export class ShopComponent implements OnInit{
         return product.price.activePrice >= this.filters.price.min && product.price.activePrice < this.filters.price.max;
       });
     }
-  }
-  
+    this.products = this.products.sort((a,b)=>{
+      if(this.filters.sortBy === "price-asc"){
+        return a.price.activePrice - b.price.activePrice;
+      }else if(this.filters.sortBy === "price-desc"){
+        return b.price.activePrice - a.price.activePrice;
+      }else if(this.filters.sortBy === "latest"){
+        return b.id - a.id;
+      }else if(this.filters.sortBy === "rating"){
+        return b.averageRating - a.averageRating;
+      }
+      return 0;
+    });
+    this.productsLength = this.products.length;
+    this.minProductIndex = (this.activePage-1)*8 + 1;
+    this.maxProductIndex = this.products.slice((this.activePage-1)*8,this.activePage*8).length + (this.activePage-1)*8;
+    this.pages = [];
+    for(let i = 1; i <= Math.ceil(this.products.length/8); i++){
+      this.pages.push(i);
+    }
+    this.products = this.products.slice((this.activePage-1)*8,this.activePage*8);
+  } 
 }
